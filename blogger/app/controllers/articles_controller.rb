bac0bff31edf2a2f23c2f0bc4acb6e873e8b3f5c
Craig <cweigle1@ivytech.edu>
2017-02-23 21:00:39 -0600

    form-based workflow feature completed

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -16,4 +16,19 @@ end
 def article_params
   params.require(:article).permit(:title, :body)
 end
+def destroy
+  Article.destroy(params[:id])
+  redirect_to articles_path(@article)
+end
+def edit
+  @article = Article.find(params[:id])
+end
+def update
+  @article = Article.find(params[:id])
+  @article.update(article_params)
+
+  flash.notice = "Article '#{@article.title}' Updated!"
+
+  redirect_to article_path(@article)
+end
 end
